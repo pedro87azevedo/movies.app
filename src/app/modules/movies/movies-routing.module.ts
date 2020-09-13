6c46ce0ecb022b82e0d80e4982d934039a1b0d8e
Pedro Azevedo <pedro87azevedo@gmail.com>
2020-09-13 18:25:14 -0300

    criando compenentes movies

---
+++ src/app/modules/movies/movies-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+
+import { MoviesComponent} from "./movies.component"
+
+const routes: Routes = [
+{
+  path: '',
+  component: MoviesComponent
+}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MoviesRoutingModule { }
