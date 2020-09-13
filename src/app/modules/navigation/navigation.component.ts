4393a23f9b23b95318035e529166590fee60390d
Pedro Azevedo <pedro87azevedo@gmail.com>
2020-09-13 02:40:08 -0300

    Implementação inicial do componente navigation

--- src/app/modules/navigation/navigation.component.ts
+++ src/app/modules/navigation/navigation.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  title: string =  'Meus Filmes Favoritos'
+
   constructor() { }
 
   ngOnInit(): void {
