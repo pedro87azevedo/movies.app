1024c19927ad14482509366c3c695088a92ff53c
Pedro Azevedo <pedro87azevedo@gmail.com>
2020-09-23 01:34:11 -0300

    formatando a data

--- src/app/modules/movies/new-movie/new-movie.component.ts
+++ src/app/modules/movies/new-movie/new-movie.component.ts
@@ -7,6 +7,7 @@ import { Ator } from "./../../../core/models/ator.model"
 import { ActorsService } from "./../../../core/services/actors.service"
 import { MyToastrService } from "./../../../core/services/toastr.service"
 import { MoviesService } from "./../../../core/services/movies.service"
+import * as moment from "moment"
 
 @Component({
   selector: 'app-new-movie',
@@ -109,6 +110,7 @@ export class NewMovieComponent implements OnInit, OnDestroy {
   }
 
   createNewMovie(): void {
+    this.setDateFormattedOnMovieForm(this.movieFormGroup.value['dataLancamento'])
     this.httpRequest = this.moviesService.createNewMovie(this.movieFormGroup.value).subscribe(response => {
       this.toastr.showToastrSuccess(`O filme ${response.body['data']['nome']} foi adicionado com sucesso`)
       this.dialogRef.close(true)
@@ -121,4 +123,12 @@ export class NewMovieComponent implements OnInit, OnDestroy {
   closeDialog(): void {
     this.dialogRef.close(false)
   }
+
+  setDateFormattedOnMovieForm(value: string): void{
+    if(value){
+      let dateFormatted: string = moment.utc(value).local().format('YYYY-MM-DD')
+      this.movieFormGroup.controls['dataLancamento'].setValue(dateFormatted)
+    }
+  }
+
 }
